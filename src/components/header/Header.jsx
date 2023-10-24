c8ba5001e294a83aa3c8f0129505d2b97163880f
Narek-Saribekyan <145029811+Narek-Saribekyan@users.noreply.github.com>
2023-10-24 22:23:15 +0400

    searchBar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom"
 import logo from "../../assets/logoImg.svg"
 import "./header.css"
+import { useState } from "react";
+
+
 const Header = () => {
+
+    const [search, setSearch] = useState("")
+
     return (
         <header className="header">
             <div className="container">
@@ -27,12 +33,41 @@ const Header = () => {
                     </nav>
                     <div className="header__buttons">
                         <div className="header__search">
-                            <input type="text" placeholder="Search"/>
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
                         </div>
                         <div className="header__button">
                             <button>Register</button>
                         </div>
+                        <div className={search.length > 0 ? "searchResult active" : "searchResult"}>
+                            <ul className="searchList">
+                                <Link className="searchList__link" >
+                                    <li className="searchItem">
+                                        <img src={logo} alt="" />
+                                        <h2>Barbershop name</h2>
+                                    </li>
+                                </Link>
+                                <Link className="searchList__link" >
+                                    <li className="searchItem">
+                                        <img src={logo} alt="" />
+                                        <h2>Barbershop name</h2>
+                                    </li>
+                                </Link>
+                                <Link className="searchList__link" >
+                                    <li className="searchItem">
+                                        <img src={logo} alt="" />
+                                        <h2>Barbershop name</h2>
+                                    </li>
+                                </Link>
+                            </ul>
+                        </div>
+
                     </div>
+
                 </div>
 
             </div>
