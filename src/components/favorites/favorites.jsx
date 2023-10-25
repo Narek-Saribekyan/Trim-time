52a1fa48c6bda34f857f29f5fd961d20c08980cb
Narek-Saribekyan <145029811+Narek-Saribekyan@users.noreply.github.com>
2023-10-25 21:52:17 +0400

    favs

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -1,28 +1,74 @@
 import { Link } from "react-router-dom";
+import "./favorites.css"
+import logo from "./logo.jpg"
 const Favorites = () => {
     const favorites = [{
         name: "Barberhop",
-        barbershop_photo: "",
-    }];
+        barbershop_photo: logo,
+        from: 10,
+        to: 10
+    },
+    {
+        name: "Barberhop",
+        barbershop_photo: logo,
+        from:10,
+        to:10
+    },
+    {
+        name: "Barberhop",
+        barbershop_photo: logo,
+        from:10,
+        to:10
+    },
+    {
+        name: "Barberhop",
+        barbershop_photo: logo,
+        from:10,
+        to:10
+    },
+    {
+        name: "Barberhop",
+        barbershop_photo: logo,
+        from:10,
+        to:10
+    },
+    {
+        name: "Barberhop",
+        barbershop_photo: logo,
+        from:10,
+        to:10
+    },
+    {
+        name: "Barberhop",
+        barbershop_photo: logo,
+        from:10,
+        to:10
+    },
+    ];
     return (
         <section className="favs">
-            <h1>Favorites</h1>
-            {favorites.length > 0 ? (
-                favorites.map(e => (
-                    <div className="favs__card card" key={e.id}>
-                        <Link>
-                            <div className="card__content">
-                                <h2>{e.name}</h2>
-                                <img src={e.barbershop_photo} alt="" />
+            <div className="container">
+                <h1 className="favs__title">Favorites</h1>
+                {favorites.length > 0 ? (
+                    <div className="favs__row">
+                        {favorites.map((e) => (
+                            <div className="favs__card card" key={e.id}>
+                                <Link to={`/details/${e.id}`}>
+                                    <div className="card__content">
+                                        <h2>{e.name}</h2>
+                                        <img src={e.barbershop_photo} alt="" />
+                                        <span>{e.from}-{e.to}</span>
+                                    </div>
+                                </Link>
                             </div>
-                        </Link>
+                        ))}
                     </div>
-                ))
-            ) : (
-                <div className="empthyFavs">
-                    <h1>You haven't choose yet</h1>
-                </div>
-            )}
+                ) : (
+                    <div className="emptyFavs">
+                        <h1>You haven't chosen yet</h1>
+                    </div>
+                )}
+            </div>
         </section>
     );
 };
